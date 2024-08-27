33984996b37b3c193e9310019718c71a4400bb94
Mignon Pelletier <code@funwithsoftware.org>
2024-08-27 13:25:43 -0700

    Implement new function: getVersion

--- prog-src/hs-plasma.hs
+++ prog-src/hs-plasma.hs
@@ -1,3 +1,4 @@
+import Control.Monad
 import Text.Printf
 
 import System.Loam.Version
@@ -7,3 +8,7 @@ main = do
   w <- x86Features
   let s = printf "%016x" w
   putStrLn s
+
+  forM_ [minBound .. maxBound] $ \v -> do
+    t <- getVersion v
+    putStrLn $ printf "%-18s = %s" (show v) t
--- src/System/Loam/Version.hs
+++ src/System/Loam/Version.hs
@@ -11,11 +11,25 @@ module System.Loam.Version
   ( VersionOfWhat(..)
   , SystemInfo(..)
   , x86Features
+  , getVersion
   ) where
 
+import qualified Data.ByteString.Unsafe as B
+import qualified Data.Text              as T
+import qualified Data.Text.Encoding     as T
 import Data.Word
+import Foreign.C
 
 import System.Loam.Internal.Enums
 
 foreign import capi "libLoam/c/ob-vers.h ob_x86_features"
     x86Features :: IO Word64
+
+foreign import capi "libLoam/c/ob-vers.h ob_get_version"
+    c_get_version :: CInt -> IO CString
+
+mallocStr2txt :: CString -> IO T.Text
+mallocStr2txt cs = T.decodeUtf8Lenient <$> B.unsafePackMallocCString cs
+
+getVersion :: VersionOfWhat -> IO T.Text
+getVersion v = c_get_version (versionOfWhat2int v) >>= mallocStr2txt
