19904ae4d17fabe5f04feccddb5917d42e0f9fbd
Mignon Pelletier <code@funwithsoftware.org>
2024-09-01 17:07:21 -0700

    Work in progress: searchStandardPath

--- src/System/Loam/Dirs.hs
+++ src/System/Loam/Dirs.hs
@@ -17,19 +17,22 @@ module System.Loam.Dirs
   ) where
 
 import qualified Data.ByteString          as B
+import Data.Default.Class
 import Data.Int
 import Data.List
 import Data.Maybe
 import qualified Data.Text                as T
+import qualified Data.Text.Encoding       as T
 -- import Data.Word
 import Foreign.C.Types
 import Foreign.Ptr
 import GHC.Stack
 
+import Data.Slaw
 import qualified System.Loam.Internal.ConstPtr as C
 import System.Loam.Internal.Enums
 import System.Loam.Internal.Filename
--- import System.Loam.Internal.StringMarshal
+import System.Loam.Internal.Marshal
 
 foreign import capi unsafe "libLoam/c/ob-dirs.h value OB_PATH_CHAR"
     c_path_char :: CChar
@@ -76,7 +79,8 @@ resolveStandardPath
   -> T.Text
   -> IO (Maybe f)
 resolveStandardPath sd fn spec = withFrozenCallStack $ do
-  listToMaybe <$> searchStandardPath0 sd fn spec 1
+  listToMaybe <$> searchStandardPath0 wh sd fn spec 1
+  where wh = "resolveStandardPath"
 
 searchStandardPath
   :: (HasCallStack, Filename f)
@@ -85,31 +89,44 @@ searchStandardPath
   -> T.Text
   -> IO [f]
 searchStandardPath sd fn spec =
-  withFrozenCallStack $ searchStandardPath0 sd fn spec reasonableLimit
+  withFrozenCallStack $ searchStandardPath0 wh sd fn spec reasonableLimit
   where
+    wh = "searchStandardPath"
     -- Limit the number of filenames returned, just in
     -- case things get out of hand for some reason.
     reasonableLimit = 1024
 
 searchStandardPath0
   :: (HasCallStack, Filename f)
-  => StandardDir
+  => String
+  -> StandardDir
   -> f
   -> T.Text
   -> Int64
   -> IO [f]
-searchStandardPath0 sd fn spec limit = do
-  undefined sd fn spec limit
+searchStandardPath0 wh sd fn spec limit = do
+  slaw <- searchStandardPath1 wh sd fn spec limit
+  undefined slaw
 
-{-
-  let sd'   = standardDir2int sd
-      fn'   = to8bitFn        fn
-      spec' = T.encodeUtf8    spec
-      addn  = Just            "TODO"
-      erl   = Just $ fnAsErl  fn
-  useAsCString fn' $ \fnPtr -> do
-    useAsCString spec' $ \specPtr -> do
-      withReturnedSlaw $ \lenPtr -> do
+searchStandardPath1
+  :: (HasCallStack, Filename f)
+  => String
+  -> StandardDir
+  -> f
+  -> T.Text
+  -> Int64
+  -> IO (Maybe Slaw)
+searchStandardPath1 wh sd fn spec limit = do
+  let sd'     = standardDir2int  sd
+      fn'     = to8bitFn         fn
+      spec'   = T.encodeUtf8     spec
+      addn    = Just             wh
+      erl     = Just $           def { elSource = DsOther erlStr  }
+      slawErl =                  def { elSource = DsOther sErlStr }
+      erlStr  = "searchspec " ++ show spec
+      sErlStr = "<internal:"  ++ wh ++ ">"
+  C.useAsConstCString fn' $ \fnPtr -> do
+    C.useAsConstCString spec' $ \specPtr -> do
+      withReturnedSlaw slawErl $ \lenPtr -> do
         withReturnedRetort EtOther addn erl $ \tortPtr -> do
-          c_search_standard_path sd' fn' spec' limit tortPtr lenPtr
--}
+          c_search_standard_path sd' fnPtr specPtr limit tortPtr lenPtr
