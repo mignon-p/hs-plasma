caf062f8269347924fefb18531a602bb70b07e3d
Mignon Pelletier <code@funwithsoftware.org>
2024-09-01 17:34:28 -0700

    Call getStandardPath from hs-plasma test program.

--- prog-src/hs-plasma.hs
+++ prog-src/hs-plasma.hs
@@ -1,6 +1,7 @@
 import Control.Monad
 import Text.Printf
 
+import System.Loam.Dirs
 import System.Loam.Version
 
 main :: IO ()
@@ -16,3 +17,12 @@ main = do
   forM_ [minBound .. maxBound] $ \si -> do
     i <- getSystemInfo si
     putStrLn $ printf "%-20s = %d" (show si) i
+
+  putStrLn ""
+
+  forM_ [minBound .. maxBound] $ \sd -> do
+    mFn <- getStandardPath sd
+    let fn = case mFn of
+               Nothing -> "<Nothing>" :: String
+               Just fn' -> fn'
+    putStrLn $ printf "%-20s = %s" (show sd) fn
