b6213cf26417be722143ae2b8a2c12cbc599dd82
Mignon Pelletier <code@funwithsoftware.org>
2024-12-20 14:05:05 -0800

    Documentation for System.Loam.Time

--- src/System/Loam/Time.hs
+++ src/System/Loam/Time.hs
@@ -5,20 +5,24 @@ Copyright   : © Mignon Pelletier, 2024
 License     : MIT
 Maintainer  : code@funwithsoftware.org
 Portability : GHC
+
+Functions for getting, formatting, and converting the time.
 -}
 
 module System.Loam.Time
-  ( LoamTime
-  , MonotonicTime
+  ( -- * Real time
+    LoamTime
   , currentTime
-  , monotonicTime
   , formatTime
   , parseTime
-    --
+    -- ** Converting
   , loamTimeToPosixTime
   , loamTimeToUtcTime
   , posixTimeToLoamTime
   , utcTimeToLoamTime
+    -- * Monotonic time
+  , MonotonicTime
+  , monotonicTime
   ) where
 
 -- import Control.Exception
@@ -42,13 +46,27 @@ import System.IO.Unsafe
 import qualified System.Loam.Internal.ConstPtr as C
 import System.Loam.Retorts
 
+-- | Number of seconds (which may be fractional) since the
+-- UNIX epoch.  (January 1, 1970 UTC)
 type LoamTime = Double
 
+-- | Number of nanoseconds since some fixed, but arbitrary,
+-- point in time.
 type MonotonicTime = Word64
 
+-- | Returns the current UTC time as a 'LoamTime'.
 foreign import capi unsafe "libLoam/c/ob-time.h ob_current_time"
     currentTime :: IO LoamTime
 
+-- | Returns the number of nanoseconds since some fixed, but arbitrary,
+-- point in time.  It will not be affected if the time-of-day is
+-- adjusted (e. g. by NTP or by VMware Tools).  Therefore, it is a
+-- good choice for measuring how long something takes, by calling it
+-- before and after and taking the difference.
+--
+-- Although the unit is nanoseconds, note that the resolution is
+-- probably not nanoseconds... could be as coarse as 15 milliseconds
+-- on Windows, for example.
 foreign import capi unsafe "libLoam/c/ob-time.h ob_monotonic_time"
     monotonicTime :: IO MonotonicTime
 
@@ -61,6 +79,12 @@ foreign import capi unsafe "libLoam/c/ob-time.h ob_strptime"
 bufSize :: Integral a => a
 bufSize = 112
 
+-- | Produces a textual representation of the given time,
+-- such as @Dec 20, 2024 13:30:53.63@.
+--
+-- Note: Although 'LoamTime' is in UTC, the string representation
+-- is in local time.  Therefore, the current time zone is used
+-- implicitly.
 formatTime :: LoamTime -> T.Text
 formatTime t = unsafePerformIO $ do
   allocaBytes bufSize $ \bufPtr -> do
@@ -68,6 +92,13 @@ formatTime t = unsafePerformIO $ do
     c_format_time_f bufPtr bufSize t
     T.decodeUtf8Lenient <$> B.packCString bufPtr
 
+-- | Given a string formatted with 'formatTime', such as
+-- @Dec 20, 2024 13:30:53.63@, returns the corresponding
+-- 'LoamTime'.
+--
+-- Note: Although 'LoamTime' is in UTC, the string representation
+-- is in local time.  Therefore, the current time zone is used
+-- implicitly.
 parseTime :: HasCallStack => T.Text -> Either PlasmaException LoamTime
 parseTime txt = unsafePerformIO $ do
   C.useAsConstCString (T.encodeUtf8 txt) $ \ccs -> do
@@ -81,14 +112,18 @@ parseTime txt = unsafePerformIO $ do
           exc <- retortToPlasmaException EtOther addn tort Nothing
           return $ Left $ exc { peCallstack = Just callStack }
 
+-- | Convert 'LoamTime' to 'POSIXTime'.
 loamTimeToPosixTime :: LoamTime -> POSIXTime
 loamTimeToPosixTime = realToFrac
 
+-- | Convert 'LoamTime' to 'UTCTime'.
 loamTimeToUtcTime :: LoamTime -> UTCTime
 loamTimeToUtcTime = posixSecondsToUTCTime . loamTimeToPosixTime
 
+-- | Convert 'POSIXTime' to 'LoamTime'.
 posixTimeToLoamTime :: POSIXTime -> LoamTime
 posixTimeToLoamTime = realToFrac
 
+-- | Convert 'UTCTime' to 'LoamTime'.
 utcTimeToLoamTime :: UTCTime -> LoamTime
 utcTimeToLoamTime = posixTimeToLoamTime . utcTimeToPOSIXSeconds
