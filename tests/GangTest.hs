a82307f818bb95adcbe98e1a74cf0bbb5d3b6a95
Mignon Pelletier <code@funwithsoftware.org>
2025-01-27 16:50:14 -0800

    Randomly join and leave gangs.

--- tests/GangTest.hs
+++ tests/GangTest.hs
@@ -22,6 +22,7 @@ import Test.Tasty.HUnit
 
 -- import Data.Slaw
 import Data.Slaw.Util
+import System.Loam.Rand
 import System.Plasma.Pool
 
 type GangMap = M.Map T.Text T.Text
@@ -59,8 +60,40 @@ checkPair expected (hName, gName)
   | expected == gName = Just hName
   | otherwise         = Nothing
 
+randomMove
+  :: HasCallStack
+  => RandState
+  -> Gang
+  -> Gang
+  -> GangMap
+  -> IO GangMap
+randomMove rs srcGang destGang gm = do
+  hoses <- getGangMembers srcGang
+  let nHoses = length hoses
+  idx <- randInt32 0 (fromIntegral nHoses) rs
+  let hose = hoses !! (fromIntegral idx)
+  leaveGang srcGang hose
+  addHose destGang hose gm
+
+randomMoves
+  :: RandState
+  -> Gang
+  -> Gang
+  -> Int
+  -> GangMap
+  -> (GangMap -> Assertion)
+  -> IO GangMap
+randomMoves rs gang1 gang2 !count !gm0 chk
+  | count <= 0 = return gm0
+  | otherwise  = do
+      gm1 <- randomMove rs gang1 gang2 gm0
+      chk gm1
+      randomMoves rs gang2 gang1 (count - 1) gm1 chk
+
 testGangs :: Assertion
 testGangs = do
+  rs     <- newRandState "testGangs" (Just 123454321)
+
   sharks <- newGang "Sharks"
   jets   <- newGang "Jets"
 
@@ -76,7 +109,9 @@ testGangs = do
   validateGang sharks gm2
   validateGang jets   gm2
 
-  -- TODO more stuff
+  randomMoves rs sharks jets 20 gm2 $ \gm -> do
+    validateGang sharks gm
+    validateGang jets   gm
 
   withdrawAll sharks
   withdrawAll jets
