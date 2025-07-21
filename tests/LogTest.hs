eaece327ab19cb5d53cda713734f730c642bb86d
Mignon Pelletier <code@funwithsoftware.org>
2025-07-21 14:43:09 -0700

    Got LogTest working.

--- tests/LogTest.hs
+++ tests/LogTest.hs
@@ -9,7 +9,6 @@ Portability : GHC
 
 module LogTest
   ( testLog
-  , parseLine
   ) where
 
 -- import Control.Applicative
@@ -19,12 +18,14 @@ import qualified Data.Attoparsec.Text     as A
 import Data.Char
 import Data.List
 import qualified Data.Text                as T
+import qualified Data.Text.IO             as T
 import System.Directory
 import System.IO
 import Test.Tasty.HUnit
 import Text.Printf
 
 import System.Loam.Log
+import System.Loam.Util
 
 import PlasmaTestUtil (tmpDir)
 
@@ -47,6 +48,14 @@ lns = [ line1 <> "\n" <> line2
       , line2
       , xtreme
       ]
+
+expLines :: [T.Text]
+expLines = [ "(1/2): " <> line1
+           , "(2/2): " <> line2
+           , line1
+           , line2
+           , xtreme
+           ]
 
 pfx :: T.Text
 pfx = "ESCAPE: "
@@ -103,6 +112,8 @@ testLog1 flags (fname, h) = do
   writeLines lvl id
   writeLines lvl (<> "\n")
   hSeek h AbsoluteSeek 0
+  txt <- T.hGetContents h
+  checkLines flags (T.lines txt)
 
 writeLines :: LogLevel -> (T.Text -> T.Text) -> IO ()
 writeLines lvl f = writeLines1 lvl $ map f lns
@@ -110,8 +121,60 @@ writeLines lvl f = writeLines1 lvl $ map f lns
 writeLines1 :: LogLevel -> [T.Text] -> IO ()
 writeLines1 lvl xs = forM_ myCodes $ \c -> forM_ xs (logCode lvl c)
 
-parseLine :: [LogFlag] -> LogCode -> T.Text -> T.Text -> A.Parser ()
-parseLine flags code prog msg = do
+checkLines :: [LogFlag] -> [T.Text] -> IO ()
+checkLines flags xs = do
+  let p1 = zip [1..] xs
+  p2 <- checkLines1 flags p1
+  p3 <- checkLines1 flags p2
+  0 @=? length p3
+
+checkLines1 :: [LogFlag] -> [(Int, T.Text)] -> IO [(Int, T.Text)]
+checkLines1 = checkLines2 myCodes
+
+checkLines2
+  :: [LogCode]
+  -> [LogFlag]
+  -> [(Int, T.Text)]
+  -> IO [(Int, T.Text)]
+checkLines2 []           _     pairs = return pairs
+checkLines2 (code:codes) flags pairs = do
+  rest <- checkLines3 flags code pairs
+  checkLines2 codes flags rest
+
+checkLines3
+  :: [LogFlag]
+  -> LogCode
+  -> [(Int, T.Text)]
+  -> IO [(Int, T.Text)]
+checkLines3 flags code pairs = do
+  let nExp     = length  expLines
+      (p1, p2) = splitAt nExp pairs
+      numP1    = length  p1
+  nExp @=? numP1
+  zipWithM_ (checkLine flags code) p1 expLines
+  return p2
+
+checkLine :: [LogFlag] -> LogCode -> (Int, T.Text) -> T.Text -> IO ()
+checkLine flags code (lNum, ln) expected = do
+  prog <- getProgName
+  let pl  = parseLine flags code prog
+      ctx = concat [ "line "
+                   , show lNum
+                   , ", code "
+                   , printf "%08x" code
+                   , ":"
+                   ]
+  case A.feed (A.parse pl ln) "" of
+    A.Fail leftover _ msg -> assertFailure $ mkFailMsg ctx leftover msg
+    A.Done _ actual       -> assertEqual ctx expected actual
+    _                     -> assertFailure $ ctx ++ " Unknown."
+
+mkFailMsg :: String -> T.Text -> String -> String
+mkFailMsg ctx leftover msg =
+  ctx ++ " Couldn't parse " ++ show leftover ++ ": " ++ msg
+
+parseLine :: [LogFlag] -> LogCode -> T.Text -> A.Parser T.Text
+parseLine flags code prog = do
   A.string pfx
   when (FlgShowProg `elem` flags) $ do
     A.string prog
@@ -134,7 +197,7 @@ parseLine flags code prog msg = do
     A.string "[t"
     A.skipWhile isDigit
     skipString "] "
-  skipString msg
+  A.takeText
 
 skipString :: T.Text -> A.Parser ()
 skipString = void . A.string
